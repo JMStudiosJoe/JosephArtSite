d6de6a51bb27d93761a2739073fd14076ea6ce81
Joseph <jrichardson@jmstudios.net>
2019-05-17 17:45:29 -0700

    css fixes title fix

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import Navigation from '../../compositions/Navigation/Navigation'
+import moment from 'moment'
 
 import './App.css' // https://makeawebsitehub.com/best-safe-web-fonts-html-css/
 
 class App extends Component {
     render() {
+        const fonts = ['Apple Chancery', 'Brush Script MT', 'Luminari', 'Trattatello']
+        const index = moment().utc().valueOf() % fonts.length - 1
+        
+        const titleStyle = {
+            fontFamily: `${fonts[index]}, Lato, Tahoma, Sans-Serif`
+        }
         return (
             <div className='App'>
                 <header className='App-header'>
-                    <h1 className='App-title'>Art of Joseph</h1>
+                    <h1 className='App-title' style={ titleStyle }>Art of Joseph</h1>
                 </header>
                 <Navigation />
             </div>
