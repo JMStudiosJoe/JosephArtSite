78e0ac4cad81558e15d861225deed023eca44b8c
Joseph <jrichardson@jmstudios.net>
2020-01-07 19:47:46 -0800

    better modal css

--- src/compositions/ModalPostDetails/ModalPostDetails.js
+++ src/compositions/ModalPostDetails/ModalPostDetails.js
@@ -9,7 +9,7 @@ import './ModalPostDetails.css'
 export default function ModalPostDetails(props) {
     const post = props.selectedPost
     const postDetailsMarkup = postInformationDetails(post)
-    const closeButton = <XCircle size={ 60 } className='close-button' onClick={ props.dismissModal } />
+    const closeButton = <XCircle size={ 30 } className='close-button' onClick={ props.dismissModal } />
     return (
         <div className='modal-details'>
             { closeButton }
